e21aaeaca6170c02bb2daea8781a89d518faff3b
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-16 06:19:32 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,23 +1,20 @@
 class SchoolClassesController < ApplicationController
-  def new
-    @school_class = SchoolClass.new
-  end
-  
-   def show
+  def show
     @school_class = SchoolClass.find(params[:id])
   end
 
-  def create
+  def new
     @school_class = SchoolClass.new
-    @school_class = SchoolClass.create(school_class_params)
-    # @school_class.title
-    # @school_class.room_number
-    @school_class.save
-    redirect_to school_class_path(@school_class)
   end
 
   def edit
     @school_class = SchoolClass.find(params[:id])
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def update
@@ -25,10 +22,10 @@ class SchoolClassesController < ApplicationController
     @school_class.update(school_class_params)
     redirect_to school_class_path(@school_class)
   end
-  
+
   private
-  
+
   def school_class_params
-    params.require(:school_class).permit(:title, :room_number)
+    params.require(:school_class).permit!
   end
 end
